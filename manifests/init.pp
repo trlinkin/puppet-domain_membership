c23b500fb7b844d7f6f705839a3ed749658a858b
Jarek Grzabel <jarek.grzabel@devopsguys.com>
2019-10-21 10:17:44 +0100

    unwrapping sensitive password

--- manifests/init.pp
+++ manifests/init.pp
@@ -84,7 +84,7 @@ class domain_membership (
   }
   exec { 'join_domain':
     environment => [ "Password=${this_password}" ],
-    command     => "exit (Get-WmiObject -Class Win32_ComputerSystem).JoinDomainOrWorkGroup('${domain}',${this_password}','${username}@${_user_domain}',${machine_ou},${join_options}).ReturnValue",
+    command     => "exit (Get-WmiObject -Class Win32_ComputerSystem).JoinDomainOrWorkGroup('${domain}','${this_password}.unwrap','${username}@${_user_domain}',${machine_ou},${join_options}).ReturnValue",
     unless      => "if((Get-WmiObject -Class Win32_ComputerSystem).domain -ne '${domain}'){ exit 1 }",
     provider    => powershell,
   }
